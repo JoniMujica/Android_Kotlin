103720459fc4b6f5489755a58a75cab6eef93d91
JoniMujica <jonimujica3@hotmail.com>
2022-09-21 20:45:42 -0300

    Ejercicio2

--- Ejercicio1/src/main/kotlin/Main.kt
+++ Ejercicio1/src/main/kotlin/Main.kt
@@ -2,14 +2,26 @@ fun main() {
     println("===============Indique cantidad de notas===============")
     var materias : FloatArray = FloatArray(inputInt());
     IngresarNotas(materias)
-    Mostrar(materias);3
+    Mostrar(materias);
 }
-fun Mostrar(notas:FloatArray):Unit{
+
+fun IngresarNotas(notas: FloatArray){
+    println("===============Cargar Notas===============")
+    for (i in notas.indices){
+        println("Cargar nota n°${i+1}")
+        notas[i] = inputFloat()
+    }
+}
+fun Mostrar(notas:FloatArray){
+
+    println("Promedio Total: ${Promedio(notas)}")
     if (Promedio(notas) >= 6 && !menorIgual3(notas)){
         println("APROBADO")
     }else if (menorIgual3(notas) && Promedio(notas) < 6){
         println("NO APROBADO")
     }else if(Promedio(notas) < 6 && !menorIgual3(notas)){
+        println("Recuperatorio")
+    }else if(Promedio(notas) >= 6 && menorIgual3(notas)){
         println("Recuperatorio")
     }
 }
@@ -27,17 +39,12 @@ fun inputFloat():Float{
     do {
         println("Ingrese un valor: ")
         numero = readln().toFloatOrNull()
-        if (numero == null) println("ERROR: digite un numero decimal")
-    }while (numero == null)
+        if (numero == null || numero>10 || numero < 1) println("ERROR: digite un numero decimal entre 1-10")
+    }while (numero == null || numero>10 || numero < 1)
     return numero;
 }
 
-fun IngresarNotas(notas: FloatArray){
-    println("===============Cargar Notas===============")
-    for (i in notas.indices){
-        notas[i] = inputFloat()
-    }
-}
+
 
 fun Promedio(notas:FloatArray):Float{
     var notasT:Float = 0.0f;
@@ -48,16 +55,17 @@ fun Promedio(notas:FloatArray):Float{
 }
 
 /**
- * Indica si tiene alumnos con nota menor a 3 segun el tipo de restriccion
+ * Indica si tiene alumnos con nota menor o igual a 3
  *
- * @param materias de materias
- * @return devuelve si alguno es menor a 3
+ * @param materias es el array de materias
+ * @return devuelve true si alguna nota es menor o igual a 3
  */
 fun menorIgual3(materias:FloatArray):Boolean {
+    var validar = false;
     for (i in materias.indices) {
             if (materias[i] <= 3) {
-                return true;
+                validar = true;
             }
     }
-    return false;
+    return validar;
 }
---
+++ Ejercicio2/src/main/kotlin/Main.kt
@@ -0,0 +1,94 @@
+fun main() {
+    var Prod_golosinas = arrayOf("Bubbaloo","Bon o Bon","Rhodesia","Pico Dulce");
+    var Val_golosinas : IntArray = intArrayOf(7,40,80,25);
+    Menu(Prod_golosinas,Val_golosinas)
+    CobraryDevolver(sumatoriaCobro(Val_golosinas))
+}
+fun Menu(golosinas:Array<String>,precios:IntArray){
+    println("======================MENU DE COMPRAS, Seleccione un producto=================")
+    for (i in golosinas.indices){
+        println("${i+1}-${golosinas[i]} -> $${precios[i]}")
+    }
+    println("0-Exit")
+}
+fun sumatoriaCobro(precios:IntArray):Int{
+    var num = 0;
+    var total:Int = 0;
+    do {
+        num = inputInt();
+        if (ExisteProducto(num) && num != 0){
+            total += precios[num-1]
+            println("Total a pagar: ${total}")
+        }
+    }while (num != 0 || !ExisteProducto(num))
+
+    return total;
+}
+
+fun CobraryDevolver(total:Int){
+    println("Por favor Ingrese el saldo $${total} para efectuar la compra: ")
+    var saldo = inputInt();
+    while (saldo-total < 0){
+        println("Saldo insuficiente, por favor ingrese el saldo total de la compra")
+        saldo = inputInt();
+    }
+    var vuelto = saldo-total;
+    println("Su vuelto es $${vuelto}")
+    var old = 0;
+    var contador = 0;
+    while (vuelto>=0){
+        var billete = BVuelto(vuelto)
+        vuelto-=billete
+        if(old == 0) old = billete;
+        if (old == billete){
+            contador++
+            if (vuelto == 0) break;
+        }else{
+            println("cantidad de billetes de $${old} x$contador")
+            contador = 1;
+            old = billete;
+        }
+    }
+}
+
+/**
+ * Esta funcion sirve para obtener el billete mas alto a devolver de forma ordenada
+ *
+ * @param faltante es la cantidad de vuelto restante para llegar a cero
+ * @return devuelve el billete mas alto cuyo diferencia difiere de cero
+ */
+fun BVuelto(faltante:Int):Int{
+    //var b:Int = 0;
+    var billetes:IntArray = intArrayOf(100,50,20,10,5,2,1);
+    for (i in billetes.indices){
+        if (faltante-billetes[i]>=0){
+            /*b = billetes[i]*/
+            //println("Vuelto billete -> $${billetes[i]}")
+            return billetes[i];
+        }
+    }
+    return 0;
+}
+
+fun inputInt():Int{
+    var numero:Int?;
+    do {
+        println("Ingrese un valor: ")
+        numero = readln().toIntOrNull()
+        if (numero == null) println("ERROR: digite un numero entero")
+    }while (numero == null)
+    return numero;
+}
+
+/**
+ * Esta funcion valida el Id del producto de ka expendedora de golosinas
+ * @param productoId es el valor ingresado por el usuario
+ * @return devuelve true si el producto existe
+ */
+fun ExisteProducto(productoId:Int):Boolean{
+    if (productoId < 0 || productoId > 4){
+        println("Error, producto invalido")
+        return false;
+    }
+    return true;
+}
