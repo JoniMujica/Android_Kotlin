6341203eaac079a95d062cd9986b29eaf1f116f9
JoniMujica <jonimujica3@hotmail.com>
2022-12-06 09:23:18 -0300

    Configuramos el binding

--- Clase21/app/src/main/java/com/example/clase21/MainActivity.kt
+++ Clase21/app/src/main/java/com/example/clase21/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.clase21
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.clase21.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding //lateinit para inicializar despues
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
+
+        /*val et_num1 = findViewById<EditText>(R.id.et_num1)
+        val et_num2 = findViewById<EditText>(R.id.et_num2)
+        val boton = findViewById<Button>(R.id.btn_calcular)
+        val tvResultado = findViewById<TextView>(R.id.tv_resultado)
+
+        boton.setOnClickListener {
+            val num1 = et_num1.text.toString().toInt()
+            val num2 = et_num2.text.toString().toInt()
+            tvResultado.text = "La suma es = ${num1 + num2}"*/
+
+
+        }
 }
