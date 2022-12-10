47b581c76de0674e6413543a40dc21bed732b3d8
JoniMujica <jonimujica3@hotmail.com>
2022-12-10 15:40:02 -0300

    Creamos y lanzamos otro activity

--- Clase23/app/src/main/java/com/example/clase23/MainActivity.kt
+++ Clase23/app/src/main/java/com/example/clase23/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.clase23
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.clase23.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnCambiar.setOnClickListener {
+            irOtraActividad()
+        }
+    }
+
+    private fun irOtraActividad() {
+        var intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
