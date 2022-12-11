33e036b9195c06e78cc9596d1bffd15bd015674a
JoniMujica <jonimujica3@hotmail.com>
2022-12-10 21:18:28 -0300

    Adherimos imagenes desde internet

--- clase24gatitos/app/src/main/java/com/example/clase24gatitos/MainActivity.kt
+++ clase24gatitos/app/src/main/java/com/example/clase24gatitos/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.clase24gatitos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.clase24gatitos.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //para saber que cosas se pueden hacer con picasso -> https://square.github.io/picasso
+        Picasso.get().load("https://placekitten.com/200/300").into(binding.imgGatitos)
     }
 }
