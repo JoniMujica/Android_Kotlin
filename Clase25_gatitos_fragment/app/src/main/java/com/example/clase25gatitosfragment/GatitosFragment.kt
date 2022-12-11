a302710332d9dd10dd300d7a67ed29893f089021
JoniMujica <jonimujica3@hotmail.com>
2022-12-11 10:01:14 -0300

    seteamos imagenes a la base de cuadros de img, muestra gatitos

--- Clase25_gatitos_fragment/app/src/main/java/com/example/clase25gatitosfragment/GatitosFragment.kt
+++ Clase25_gatitos_fragment/app/src/main/java/com/example/clase25gatitosfragment/GatitosFragment.kt
@@ -7,12 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import com.example.clase25gatitosfragment.databinding.FragmentGatitosBinding
+import com.squareup.picasso.Picasso
 
 
 class GatitosFragment : Fragment() {
 
     private var _binding : FragmentGatitosBinding? = null
     private val binding get() = _binding!!
+
+    val foto1 = "https://placekitten.com/200/300"
+    val foto2 = "https://placekitten.com/300/300"
+    val foto3 = "https://placekitten.com/400/300"
+    val foto4 = "https://placekitten.com/300/400"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,12 +31,21 @@ class GatitosFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentGatitosBinding.inflate(inflater,container,false)
+        loadImages()
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun loadImages(){
+        Picasso.get().load(foto1).into(binding.imgFoto1)
+        Picasso.get().load(foto2).into(binding.imgFoto2)
+        Picasso.get().load(foto3).into(binding.imgFoto3)
+        Picasso.get().load(foto4).into(binding.imgFoto4)
+
     }
 
 }
--- Clase25_gatitos_fragment/app/src/main/java/com/example/clase25gatitosfragment/MainActivity.kt
+++ Clase25_gatitos_fragment/app/src/main/java/com/example/clase25gatitosfragment/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        loadFragment(GatitosFragment())
     }
 
     private fun loadFragment(fragment: Fragment){
