d23f5d107b288612b76b4a8dd0cb25b8e1401306
JoniMujica <jonimujica3@hotmail.com>
2022-12-11 09:40:54 -0300

    realizamos la base de las imagenes e inyectamos binding

---
+++ Clase25_gatitos_fragment/app/src/main/java/com/example/clase25gatitosfragment/GatitosFragment.kt
@@ -0,0 +1,35 @@
+package com.example.clase25gatitosfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import com.example.clase25gatitosfragment.databinding.FragmentGatitosBinding
+
+
+class GatitosFragment : Fragment() {
+
+    private var _binding : FragmentGatitosBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentGatitosBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
--- Clase25_gatitos_fragment/app/src/main/java/com/example/clase25gatitosfragment/MainActivity.kt
+++ Clase25_gatitos_fragment/app/src/main/java/com/example/clase25gatitosfragment/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.clase25gatitosfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.clase25gatitosfragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun loadFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.layoutPadre.id,fragment)
+        fragmentTransaction.commit()
     }
 }
